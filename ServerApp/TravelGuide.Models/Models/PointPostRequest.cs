fe5e284e39ee803035afb47eb1a52a48348fd234
Dmitry Gorbulko <84370097+DmitriyGorbulko@users.noreply.github.com>
2024-05-17 20:39:19 +0300

    Добавлены DTO

---
+++ ServerApp/TravelGuide.Models/Models/PointPostRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelGuide.Models.Models
+{
+    public class PointPostRequest
+    {
+        public string X { get; set; }
+
+        public string Y { get; set; }
+    }
+}
